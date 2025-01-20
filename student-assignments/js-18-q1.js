5f6a9286d3ced6ad506b79499e00667b772b7229
BaharAlmasiMoghaddam <baharalmasiimoghaddam@gmail.com>
2025-01-20 17:28:20 +0330

    Done-2

--- student-assignments/js-18-q1.js
+++ student-assignments/js-18-q1.js
@@ -4,7 +4,36 @@
 //todo-3:The third parameter should be a string indicating either "add => addition" or "multiply => multiplication".
 //todo-4:The fourth parameter should be a function that log the result of the calculation.
 
-function calculate() {}
+function calculate(num1, num2, operations, logResult) {
+  if (typeof num1 !== "number" || typeof num2 !== "number") {
+    console.error("The 1st & 2nd parameters need to be numbers!");
+    return;
+  }
+
+  //
+
+  let result;
+  if (operations === "add") {
+    result = num1 + num2;
+  } else if (operations === "multiply") {
+    result = num1 * num2;
+  } else {
+    console.error("You can only use 'add' or 'multiply as the 3rd parameter!");
+    return;
+  }
+
+  //
+
+  if (typeof logResult === "function") {
+    logResult(result);
+  } else {
+    console.error("You need to call its function!");
+  }
+}
+
+function handleCalculationResult(result) {
+  console.log("The result is : " + result);
+}
 
 calculate(5, 3, "add", handleCalculationResult);
 calculate(4, 6, "multiply", handleCalculationResult);
