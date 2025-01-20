aab03330c9cd28ad5b6939a515c2c90966408dc7
BaharAlmasiMoghaddam <baharalmasiimoghaddam@gmail.com>
2025-01-20 23:42:50 +0330

    Done-lastOne

--- student-assignments/JS-18-Q4.js
+++ student-assignments/JS-18-Q4.js
@@ -6,3 +6,33 @@
   - If a negative value is entered into the function, it will be rejected.
   - Note: you should use "Promise" to resolve this question.
 */
+
+function sum(a, b) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (a < 0 || b < 0) {
+        reject("You can not use negative numbers!!");
+      } else {
+        resolve(a + b);
+      }
+    }, 500);
+  });
+}
+
+//
+
+document.getElementById("sumApp").addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const num1 = parseFloat(document.getElementById("num1").value);
+  const num2 = parseFloat(document.getElementById("num2").value);
+
+  sum(num1, num2)
+    .then((result) => {
+      document.getElementById("result").textContent =
+        "Sum is:  " + result + "  🎉";
+    })
+    .catch((error) => {
+      document.getElementById("result").textContent = "Error" + error;
+    });
+});
